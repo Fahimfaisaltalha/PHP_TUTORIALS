5bd00a26c510424fb57c5e6211e7eb205a3e0aa6
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-01-28 01:44:12 +0600

    MySQL_Create_db part

---
+++ MySQL_Create_db.php
@@ -0,0 +1,38 @@
+<?php
+// Ways to connect to a MySQL Database
+// 1. MySQLi extension
+// 2. PDO
+
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dbtalha"; // Your database name
+
+// Create a Connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connection was successful<br>";
+
+// Create a Table
+$sql = "CREATE TABLE `trip` (
+    `sno` INT(6) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(12) NOT NULL,
+    `dest` VARCHAR(6) NOT NULL,
+    PRIMARY KEY (`sno`)
+)";
+
+// Execute the query
+if (mysqli_query($conn, $sql)) {
+    echo "Table 'trip' created successfully.";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// Close the connection
+mysqli_close($conn);
+?>
