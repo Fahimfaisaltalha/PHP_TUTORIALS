a217a275ae9ccd4737e8c11be2ec46b3d8b87925
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-01-27 12:10:46 +0600

    Index part

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 echo "My First Tutorial Page";
+$a=["talha","Fahim"];
+echo $a[0]."<br>";
+function myfunction(){
+    $a=10;
+    $b=24;
+    $c= $a+$b;
+    echo $c."<br>";
+    return $c;
+}
+myfunction();
+
+$name ="My Name is Talha ";
+echo strlen($name)."<br>";
+echo "My name"."is"."Fahim Faisal"."<br>";
+echo str_word_count($name)."<br>";
+echo strrev($name)."<br>";
+echo strpos($name,"is")."<br>";
+echo str_replace("Talha","Don",$name)."<br>";
+echo str_repeat($name,10)
 
 ?>
