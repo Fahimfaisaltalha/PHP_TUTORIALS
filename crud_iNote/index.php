3a074a9b5a901a883a76bc9920b81eaef44e95cb
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-02-01 03:09:58 +0600

    Edit Option

--- crud_iNote/index.php
+++ crud_iNote/index.php
@@ -38,10 +38,43 @@ if ($result) {
   <title>iNotes - Note taking make easy</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="//cdn.datatables.net/2.2.1/css/dataTables.dataTables.min.css">
-  
+
 </head>
 
 <body>
+<!-- Edit modal -->
+<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Edit Modal
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editModal">Edit this Note</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="/PHP_TUTORIALS/crud_iNote/index.php" method="post">
+      <div class="my-3">
+        <label for="title" class="form-label">Note Title</label>
+        <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+      </div>
+      <label for="floatingTextarea2">Note Description</label>
+      <div class="form-floating">
+        <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" style="height: 100px"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary my-3">Add Note</button>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
@@ -113,14 +146,15 @@ if ($result) {
           <th scope='row'>".$sno ."</th>
           <td>".$row['title'] . "</td>
           <td>".$row['description'] ."</td>
-          <td>Action</td>
+          <td> <button class='edit btn btn-sm btn-primary'>Edit</button> <a href='/del'\>Delete</a></td>
         </tr>";
 
         }
+        
         ?>
 
 
-      </tbody>
+      </tbody><
 
     </table>
   </div>
@@ -131,6 +165,29 @@ if ($result) {
   <script>
     let table = new DataTable('#myTable');
   </script>
+<script>
+  // Select all edit buttons
+  edits = document.getElementsByClassName('edit');
+  Array.from(edits).forEach((element) => {
+    element.addEventListener("click", (e) => {
+      console.log("Edit button clicked");
+      
+      tr = e.target.closest("tr"); // Get the closest row
+      title = tr.getElementsByTagName("td")[0].innerText;
+      description = tr.getElementsByTagName("td")[1].innerText;
+      
+      console.log(title, description);
+      
+      document.getElementById("titleEdit").value = title;
+      document.getElementById("descriptionEdit").value = description;
+
+      // Open the modal
+      let myModal = new bootstrap.Modal(document.getElementById('editModal'));
+      myModal.show();
+    });
+  });
+</script>
+
 </body>
 
 </html>
