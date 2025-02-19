a63535d7f6ed4744385247c56574c4c5054cb22a
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-02-19 19:57:49 +0600

    welcome after login

--- login_system/login.php
+++ login_system/login.php
@@ -1,17 +1,71 @@
+<?php  
+$login=false;
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+    include 'partial/dbconnect.php';
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+
+    
+    
+        $sql="SELECT * from users WHERE username='$username' AND password='$password'" ;
+        $result=mysqli_query($con,$sql);
+        $num=mysqli_num_rows($result);
+        if($num==1){
+            $login = true;
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username']=$username;
+            header("location:welcome.php");
+            exit;
+        }
+    }
+
+
+
+
+?>
+
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    
+</head>
+
+<body>
+
     <?php
-        require '../login_system/partial/nav.php';
-    
+    require '../login_system/partial/nav.php'; ?>
+    <?php
+    if($login){
+    echo'<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> You are Logged in.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
     ?>
+    <div class="container">
+        <h1 class="text-center">Login to our Website</h1>
+        <form action="/PHP_TUTORIALS/login_system/login.php" method="post">
+            <div class="mb-3 ">
+                <label for="username" class="form-label">User-name</label>
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+               
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            
+
+            <button type="submit" class="btn btn-primary ">Login</button>
+        </form>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
--- login_system/welcome.php
+++ login_system/welcome.php
@@ -1,18 +1,25 @@
+<?php 
+session_start();
+if(!isset($_SESSION['loggedin'])|| $_SESSION['loggedin']!=true){
+  header("location:login.php");
+  exit;
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome</title>
+    <title>Welcome  - <?php echo $_SESSION['username']?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     
     <?php
-        require '../login_system/partial/nav.php';
+        require '../login_system/partial/nav.php'?>
+        Welcome  -  <?php echo $_SESSION['username']?>
     
-    ?>
-    <h1>Welcome Dear</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
